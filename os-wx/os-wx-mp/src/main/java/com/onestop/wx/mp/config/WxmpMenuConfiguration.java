3b26f3793294a2d5eea0e8884e00419ea6245712
changhongyuan <changhongyuan@126.com>
2021-04-25 10:51:47 +0800

    修改：微信服务号模块、创建菜单逻辑

--- os-wx/os-wx-mp/src/main/java/com/onestop/wx/mp/config/WxmpMenuConfiguration.java
+++ os-wx/os-wx-mp/src/main/java/com/onestop/wx/mp/config/WxmpMenuConfiguration.java
@@ -1,18 +1,12 @@
 package com.onestop.wx.mp.config;
 
-import cn.hutool.core.collection.CollUtil;
 import com.onestop.wx.mp.extra.entity.MenuConfigs;
-import me.chanjar.weixin.mp.api.WxMpService;
-import me.chanjar.weixin.mp.api.impl.WxMpServiceImpl;
-import me.chanjar.weixin.mp.config.impl.WxMpDefaultConfigImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.Map;
 
 /**
  * os-wx-mp配置

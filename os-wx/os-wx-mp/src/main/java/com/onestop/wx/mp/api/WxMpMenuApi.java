1cea12252199bc67f721e5cf398d64e3d2e7030f
changhongyuan <changhongyuan@126.com>
2021-04-25 17:33:14 +0800

    修改：微信服务号模块、调整starter模块yml配置路径，apiPath等默认值配置

--- os-wx/os-wx-mp/src/main/java/com/onestop/wx/mp/api/WxMpMenuApi.java
+++ os-wx/os-wx-mp/src/main/java/com/onestop/wx/mp/api/WxMpMenuApi.java
@@ -5,7 +5,6 @@ import com.onestop.common.core.util.Res;
 import com.onestop.wx.mp.extra.dto.MenuConfigs;
 import com.onestop.wx.mp.util.OsWxMpUtils;
 import lombok.extern.slf4j.Slf4j;
-import me.chanjar.weixin.common.error.WxErrorException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,19 +21,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("${os.wxmp.apiPath}/menu")
 public class WxMpMenuApi {
-    @Autowired
+    @Autowired(required = false)
     private MenuConfigs menuConfigs;
     @Autowired
     private OsWxMpUtils osWxMpUtils;
 
     @GetMapping("/create")
     public Res create() {
-         try {
-             this.osWxMpUtils.menuCreate(this.menuConfigs);
+        try {
+            this.osWxMpUtils.menuCreate(this.menuConfigs);
             return Res.ok();
-         } catch (BizException e) {
-             return Res.failed(e.getMsg());
-         }
+        } catch (BizException e) {
+            return Res.failed(e.getMsg());
+        } catch (NullPointerException e) {
+            return Res.failed("配置menu.isopen未启用");
+        }
     }
 
     @PostMapping("/create")

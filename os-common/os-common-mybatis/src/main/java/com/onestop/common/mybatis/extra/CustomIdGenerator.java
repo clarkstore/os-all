2f7e0ce8b862f1ec167c79492efd66115d1c5093
changhongyuan <changhongyuan@126.com>
2021-05-26 17:50:10 +0800

    修改：mybatis模块-自定义ID生成器

--- os-common/os-common-mybatis/src/main/java/com/onestop/common/mybatis/extra/CustomIdGenerator.java
+++ os-common/os-common-mybatis/src/main/java/com/onestop/common/mybatis/extra/CustomIdGenerator.java
@@ -1,33 +1,22 @@
 package com.onestop.common.mybatis.extra;
 
+import cn.hutool.core.util.IdUtil;
 import com.baomidou.mybatisplus.core.incrementer.IdentifierGenerator;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.ibatis.reflection.MetaObject;
-import org.apache.ibatis.reflection.SystemMetaObject;
 import org.springframework.stereotype.Component;
-
-import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * 自定义ID生成器
  *
- * @author nieqiuqiu 2019/11/30
+ * @author Clark
+ * @version 2021-05-26
  */
 @Slf4j
 @Component
 public class CustomIdGenerator implements IdentifierGenerator {
 
-    private final AtomicLong al = new AtomicLong(1);
-
     @Override
     public Long nextId(Object entity) {
-        //可以将当前传入的class全类名来作为bizKey,或者提取参数来生成bizKey进行分布式Id调用生成.
-        String bizKey = entity.getClass().getName();
-        log.info("bizKey:{}", bizKey);
-        MetaObject metaObject = SystemMetaObject.forObject(entity);
-        String name = (String) metaObject.getValue("name");
-        final long id = al.getAndAdd(1);
-        log.info("为{}生成主键值->:{}", name, id);
-        return id;
+        return IdUtil.getSnowflake(1, 1).nextId();
     }
 }

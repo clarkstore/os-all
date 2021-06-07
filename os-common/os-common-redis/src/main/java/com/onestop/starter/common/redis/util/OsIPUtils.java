40678e5fe6d56258578734851f23bd2b7eb114d3
changhongyuan <changhongyuan@126.com>
2021-06-07 15:54:11 +0800

    修改：IPUtils优化代码

--- os-common/os-common-redis/src/main/java/com/onestop/starter/common/redis/util/OsIPUtils.java
+++ os-common/os-common-redis/src/main/java/com/onestop/starter/common/redis/util/OsIPUtils.java
@@ -18,7 +18,11 @@
 
 package com.onestop.starter.common.redis.util;
 
+import lombok.extern.slf4j.Slf4j;
+
 import javax.servlet.http.HttpServletRequest;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 /**
  * IP工具类
@@ -26,6 +30,7 @@ import javax.servlet.http.HttpServletRequest;
  * @author Clark
  * @version 2021/5/12
  */
+@Slf4j
 public class OsIPUtils {
 
     private static final String UNKNOWN = "unknown";
@@ -38,6 +43,7 @@ public class OsIPUtils {
      * @param request
      * @return IP地址
      */
+    @Deprecated
     public static String getIpAddr(HttpServletRequest request) {
         String ip = request.getHeader("x-forwarded-for");
         if (ip == null || ip.length() == 0 || UNKNOWN.equalsIgnoreCase(ip)) {
@@ -50,5 +56,67 @@ public class OsIPUtils {
             ip = request.getRemoteAddr();
         }
         return "0:0:0:0:0:0:0:1".equals(ip) ? "127.0.0.1" : ip;
+    }
+
+    /**
+     * 获取客户端的IP地址<br/>
+     * 注意本地测试访问项目地址时，浏览器请求不要用 localhost，请用本机IP；否则，取不到 IP
+     * @param request
+     * @return IP地址
+     */
+    public static String getClientIpAddress(HttpServletRequest request) {
+        // 获取请求主机IP地址,如果通过代理进来，则透过防火墙获取真实IP地址
+        String headerName = "x-forwarded-for";
+        String ip = request.getHeader(headerName);
+        if (null != ip && ip.length() != 0 && !UNKNOWN.equalsIgnoreCase(ip)) {
+            // 多次反向代理后会有多个IP值，第一个IP才是真实IP,它们按照英文逗号','分割
+            if (ip.indexOf(",") != -1) {
+                ip = ip.split(",")[0];
+            }
+        }
+        if (checkIp(ip)) {
+            headerName = "Proxy-Client-IP";
+            ip = request.getHeader(headerName);
+        }
+        if (checkIp(ip)) {
+            headerName = "WL-Proxy-Client-IP";
+            ip = request.getHeader(headerName);
+        }
+        if (checkIp(ip)) {
+            headerName = "HTTP_CLIENT_IP";
+            ip = request.getHeader(headerName);
+        }
+        if (checkIp(ip)) {
+            headerName = "HTTP_X_FORWARDED_FOR";
+            ip = request.getHeader(headerName);
+        }
+        if (checkIp(ip)) {
+            headerName = "X-Real-IP";
+            ip = request.getHeader(headerName);
+        }
+        if (checkIp(ip)) {
+            headerName = "remote addr";
+            ip = request.getRemoteAddr();
+            // 127.0.0.1 ipv4, 0:0:0:0:0:0:0:1 ipv6
+            if ("127.0.0.1".equals(ip) || "0:0:0:0:0:0:0:1".equals(ip)) {
+                //根据网卡取本机配置的IP
+                InetAddress inet = null;
+                try {
+                    inet = InetAddress.getLocalHost();
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                }
+                ip = inet.getHostAddress();
+            }
+        }
+        log.error("getClientIp  IP is " + ip + ", headerName = " + headerName);
+        return ip;
+    }
+
+    private static boolean checkIp(String ip) {
+        if (null == ip || ip.length() == 0 || UNKNOWN.equalsIgnoreCase(ip)) {
+            return true;
+        }
+        return false;
     }
 }

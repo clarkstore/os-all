806ad85aa332c23aa28dbadd17e008e415c8932a
Clark <changhongyuan@126.com>
2021-09-09 13:48:07 +0800

    修改：redis模块LocalDateTime解决Redis序列化异常的问题

--- os-starter/os-redis-spring-boot-starter/src/main/java/com/onestop/starter/common/redis/config/OsRedisAutoConfiguration.java
+++ os-starter/os-redis-spring-boot-starter/src/main/java/com/onestop/starter/common/redis/config/OsRedisAutoConfiguration.java
@@ -19,8 +19,12 @@
 package com.onestop.starter.common.redis.config;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.onestop.starter.common.redis.util.OsRedisUtils;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -51,7 +55,10 @@ public class OsRedisAutoConfiguration {
         Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
         ObjectMapper om = new ObjectMapper();
         om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        om.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
+        // LocalDateTime解决Redis序列化异常的问题
+        om.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        om.registerModule(new JavaTimeModule());
         jackson2JsonRedisSerializer.setObjectMapper(om);
 
         StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();

dd830f060c052fdd11fdefd7eff010922540e487
Clark <changhongyuan@126.com>
2021-11-10 13:19:42 +0800

    修改：mybatis-plus模块加载问题

--- os-common/os-common-mybatis/src/main/java/com/onestop/common/mybatis/extra/CustomIdGenerator.java
+++ os-common/os-common-mybatis/src/main/java/com/onestop/common/mybatis/extra/CustomIdGenerator.java
@@ -21,7 +21,6 @@ package com.onestop.common.mybatis.extra;
 import cn.hutool.core.util.IdUtil;
 import com.baomidou.mybatisplus.core.incrementer.IdentifierGenerator;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
 
 /**
  * 自定义ID生成器
@@ -30,7 +29,6 @@ import org.springframework.stereotype.Component;
  * @version 2021-05-26
  */
 @Slf4j
-@Component
 public class CustomIdGenerator implements IdentifierGenerator {
 
     @Override

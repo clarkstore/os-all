0affec8cc59531797f8054623f897b61ea9f4f90
Clark <changhongyuan@126.com>
2022-01-24 15:03:04 +0800

    修改：starter模块命名

---
+++ os-starter/os-task-spring-boot-starter/src/main/java/com/onestop/common/task/autoconfigure/OsTaskSchedulerAutoConfiguration.java
@@ -0,0 +1,51 @@
+/*
+ *
+ *  * Copyright (c) 2021 os-parent Authors. All Rights Reserved.
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *     http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+
+package com.onestop.common.task.autoconfigure;
+
+import com.onestop.common.task.util.OsTaskSchedulerUtils;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+/**
+ * os-common-task配置
+ * @author Clark
+ * @version 2021-04-08
+ */
+@Configuration
+@ConditionalOnProperty(prefix = "os.task", name = "enabled", havingValue = "true")
+public class OsTaskSchedulerAutoConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public ThreadPoolTaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
+        return taskScheduler;
+    }
+
+    @Bean
+    @ConditionalOnBean(ThreadPoolTaskScheduler.class)
+    public OsTaskSchedulerUtils osTaskSchedulerUtils() {
+        OsTaskSchedulerUtils schedulerUtils = new OsTaskSchedulerUtils();
+        return schedulerUtils;
+    }
+}

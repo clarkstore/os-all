8a7c9229b8ac113d953800fcf68e942f1705a080
Clark <changhongyuan@126.com>
2022-03-03 15:29:47 +0800

    追加：日志starter模块

---
+++ os-common/os-common-log/src/main/java/com/onestop/common/log/mapper/BizLogMapper.java
@@ -0,0 +1,16 @@
+package com.onestop.common.log.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.onestop.common.log.model.entity.BizLog;
+
+/**
+ * <p>
+ * 接口日志表 Mapper 接口
+ * </p>
+ *
+ * @author Clark
+ * @version 2022-03-03
+ */
+public interface BizLogMapper extends BaseMapper<BizLog> {
+
+}

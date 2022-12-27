2159eaf769e913c1d52d3a67d1a341be3fecd447
Clark <changhongyuan@126.com>
2022-12-27 15:56:14 +0800

    修改：优化统一处理controller返回加密逻辑

--- os-common/os-common-web/src/main/java/com/onestop/common/web/config/OsResAesBodyAdvice.java
+++ os-common/os-common-web/src/main/java/com/onestop/common/web/config/OsResAesBodyAdvice.java
@@ -28,6 +28,7 @@ import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.http.MediaType;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
 import java.lang.reflect.AnnotatedElement;
@@ -36,10 +37,13 @@ import java.lang.reflect.AnnotatedElement;
  * 实现统一处理controller返回
  *
  * @author Clark
- * @version 2020-09-09
+ * @version 2022-12-27
  */
-//@RestControllerAdvice("com.onestop")
-public abstract class OsResponseBodyAdvice implements ResponseBodyAdvice<Object> {
+@RestControllerAdvice("com.onestop")
+public class OsResAesBodyAdvice implements ResponseBodyAdvice<Object> {
+    /**
+     * 加密工具类
+     */
     @Autowired(required = false)
     protected OsAesUtils aesUtils;
 

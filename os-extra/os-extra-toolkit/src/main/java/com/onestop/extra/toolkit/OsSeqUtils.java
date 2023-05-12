45a1207ae3f283b18f796870f3fc67c3a940b0f0
chang <changhongyuan@126.com>
2023-05-12 10:19:41 +0800

    修改：redis模块取消redisson引入，流水号工具类不再依赖redis

--- os-extra/os-extra-toolkit/src/main/java/com/onestop/extra/toolkit/OsSeqUtils.java
+++ os-extra/os-extra-toolkit/src/main/java/com/onestop/extra/toolkit/OsSeqUtils.java
@@ -20,20 +20,15 @@ package com.onestop.extra.toolkit;
 
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.map.MapUtil;
-import cn.hutool.core.thread.ThreadUtil;
 import cn.hutool.core.util.StrUtil;
-import com.onestop.common.redis.util.OsRedisUtils;
-import com.onestop.common.redis.util.OsRedissonUtils;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
-import org.redisson.api.RLock;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.LocalDateTime;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
@@ -46,28 +41,34 @@ import java.util.concurrent.locks.ReentrantLock;
 public class OsSeqUtils {
     /**
      * 记录已有流水单
+     * 允许设置初始值
      */
+    @Setter
     private long seqNo = 1;
     /**
      * 配置参数
      */
-    private Map<String, String> map = MapUtil.newHashMap();
+    private Map<String, String> map = MapUtil.newConcurrentHashMap();
+    /**
+     * map记录当前日期seq最大值key
+     */
+    private static final String MAP_SEQ_KEY = "date";
     private static final ReentrantLock REENTRANT_LOCK = new ReentrantLock();
     /**
      * redis缓存24小时过期
      */
-    private static final long REDIS_DURATION_SECONDS = 60 * 60 * 24;
+//    private static final long REDIS_DURATION_SECONDS = 60 * 60 * 24;
     /**
      * redis分布式锁key
      */
-    private static final String REDIS_LOCK_KEY = "seqLock:";
+//    private static final String REDIS_LOCK_KEY = "seqLock:";
     /**
      * 配置缓存可以根据不同业务类型分别创建序列号
      */
     @Autowired(required = false)
-    private OsRedisUtils redisUtils;
-    @Autowired(required = false)
-    private OsRedissonUtils redissonUtils;
+//    private OsRedisUtils redisUtils;
+//    @Autowired(required = false)
+//    private OsRedissonUtils redissonUtils;
     private OsSeqConfig seqConfig;
     public OsSeqUtils(OsSeqConfig seqConfig) {
         this.seqConfig = seqConfig;
@@ -78,81 +79,97 @@ public class OsSeqUtils {
      * @return
      */
     public String getSeqNo() {
-        // 使用redis
-        if (this.seqConfig.isUseRedis()) {
-            return this.getSeqByRedis(StrUtil.EMPTY);
-        }
+//        // 使用redis
+//        if (this.seqConfig.isUseRedis()) {
+//            return this.getSeqByRedis(StrUtil.EMPTY);
+//        }
         return this.getSeq(StrUtil.EMPTY);
     }
 
-    long tid;
+    /**
+     * 取流水号
+     * @return
+     */
+    public String getSeqNo(String bizFlag) {
+//        // 使用redis
+//        if (this.seqConfig.isUseRedis()) {
+//            return this.getSeqByRedis(bizFlag);
+//        }
+        return this.getSeq(bizFlag);
+    }
+
     /**
      * 根据业务标识取流水号
      * @param bizFlag
      * @return
      */
-    public String getSeqByRedis(String bizFlag) {
-        long seq = 1;
-        String todayStr = DateUtil.format(LocalDateTime.now(), this.seqConfig.getDateFormat());
-        this.map.put("bizFlag", bizFlag);
-        this.map.put("date", todayStr);
-
-        String redisKey = StrUtil.format(OsSeqConfig.RedisKeyTemplate, this.map);
-        String lockKey = REDIS_LOCK_KEY + bizFlag;
-
-        try {
-            boolean isGet;
-            RLock lock = this.redissonUtils.fairLock(lockKey);
-            do {
-                //加锁
-                isGet = this.redissonUtils.tryLock(lock, TimeUnit.MILLISECONDS, 1, 5000);
-                if (isGet) {
-                    if (this.redisUtils.hasKey(redisKey)) {
-                        seq = this.redisUtils.incr(redisKey);
-                        log.error("----------seq---------" + seq);
-                    } else {
-                        // 缓存24小时
-                        this.redisUtils.set(redisKey, seq, REDIS_DURATION_SECONDS);
-                    }
-                } else {
-                    ThreadUtil.sleep(40);
-                }
-            } while (!isGet);
-        } finally {
-            //释放锁
-            this.redissonUtils.unlock(lockKey);
-        }
-
-        String seqStr = StrUtil.fillBefore(String.valueOf(seq), '0', this.seqConfig.getSeqLength());
-        map.put("seq", seqStr);
-
-        return StrUtil.format(this.seqConfig.getTemplate(), this.map, false);
-    }
+//    private String getSeqByRedis(String bizFlag) {
+//        long seq = 1;
+//        String todayStr = DateUtil.format(LocalDateTime.now(), this.seqConfig.getDateFormat());
+//        this.map.put("bizFlag", bizFlag);
+//        this.map.put("date", todayStr);
+//
+//        String redisKey = StrUtil.format(OsSeqConfig.RedisKeyTemplate, this.map);
+//        String lockKey = REDIS_LOCK_KEY + bizFlag;
+//
+//        try {
+//            boolean isGet;
+//            RLock lock = this.redissonUtils.fairLock(lockKey);
+//            do {
+//                //加锁
+//                isGet = this.redissonUtils.tryLock(lock, TimeUnit.MILLISECONDS, 1, 5000);
+//                if (isGet) {
+//                    if (this.redisUtils.hasKey(redisKey)) {
+//                        seq = this.redisUtils.incr(redisKey);
+//                        log.error("----------seq---------" + seq);
+//                    } else {
+//                        // 缓存24小时
+//                        this.redisUtils.set(redisKey, seq, REDIS_DURATION_SECONDS);
+//                    }
+//                } else {
+//                    ThreadUtil.sleep(40);
+//                }
+//            } while (!isGet);
+//        } finally {
+//            //释放锁
+//            this.redissonUtils.unlock(lockKey);
+//        }
+//
+//        String seqStr = StrUtil.fillBefore(String.valueOf(seq), '0', this.seqConfig.getSeqLength());
+//        map.put("seq", seqStr);
+//
+//        return StrUtil.format(this.seqConfig.getTemplate(), this.map, false);
+//    }
 
     /**
      * 取得流水号
+     * 流水号不因bizFlag不同而单独累计
      * @param bizFlag
      * @return
      */
-    public String getSeq(String bizFlag) {
+    private String getSeq(String bizFlag) {
+        String todayStr = DateUtil.format(LocalDateTime.now(), this.seqConfig.getDateFormat());
         //上锁
         REENTRANT_LOCK.lock();
         try {
-            String todayStr = DateUtil.format(LocalDateTime.now(), this.seqConfig.getDateFormat());
-            if (todayStr.equals(this.map.get("date"))) {
+            if (todayStr.equals(this.map.get(MAP_SEQ_KEY))) {
                 //当日流水号+1
                 this.seqNo += 1;
             } else {
                 //重置流水号
                 this.seqNo = 1;
             }
-            this.map.put("bizFlag", bizFlag);
-            this.map.put("date", todayStr);
+            // 保存当前日期，判断是否有新起始一天
+            this.map.put(MAP_SEQ_KEY, todayStr);
+
+            Map<String, String> param = MapUtil.newHashMap();
+            param.put("bizFlag", bizFlag);
+            param.put("date", todayStr);
 
             String seqStr = StrUtil.fillBefore(String.valueOf(this.seqNo), '0', this.seqConfig.getSeqLength());
-            this.map.put("seq", seqStr);
+            param.put("seq", seqStr);
 
-            return StrUtil.format(this.seqConfig.getTemplate(), this.map, false);
+            return StrUtil.format(this.seqConfig.getTemplate(), param, false);
         } finally {
             //释放锁
             if (REENTRANT_LOCK.isLocked()) {
--- os-starter/os-redis-spring-boot-starter/src/main/java/com/onestop/common/redis/autoconfigure/OsRedisAutoConfiguration.java
+++ os-starter/os-redis-spring-boot-starter/src/main/java/com/onestop/common/redis/autoconfigure/OsRedisAutoConfiguration.java
@@ -26,12 +26,11 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.onestop.common.redis.util.OsRedisUtils;
-import com.onestop.common.redis.util.OsRedissonUtils;
-import org.redisson.spring.data.connection.RedissonConnectionFactory;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.DependsOn;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -47,7 +46,7 @@ public class OsRedisAutoConfiguration {
 
     @Bean
     @ConditionalOnMissingBean
-    public RedisTemplate<String, Object> redisTemplate(RedissonConnectionFactory factory) {
+    public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory factory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(factory);
 
@@ -79,13 +78,6 @@ public class OsRedisAutoConfiguration {
     @ConditionalOnMissingBean
     public OsRedisUtils osRedisUtils() {
         OsRedisUtils utils = new OsRedisUtils();
-        return utils;
-    }
-
-    @Bean
-    @ConditionalOnMissingBean
-    public OsRedissonUtils osRedissonUtils() {
-        OsRedissonUtils utils = new OsRedissonUtils();
         return utils;
     }
 }
